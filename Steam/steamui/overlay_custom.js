09bfd6da1a66760c2ae07a04ad8a071358ae0007
Thespikedballofdoom <43627353+Thespikedballofdoom@users.noreply.github.com>
2023-10-01 14:39:12 -0400

    Add overlay_custom.js

---
+++ Steam/steamui/overlay_custom.js
@@ -0,0 +1,20 @@
+//needs to be loaded with javascript in SFP
+//Redefine this so its always our player
+let GetCommunityDiscussionForumsURL = function(){ return "http://:jart@localhost:8776/";};
+
+function Musicsupersize() {
+	if (window.name.includes("OverlayBrowser_Discussions")) {
+		console.log("we detected overlaybrowser_discussions, changing window size...");
+		SteamClient.Window.SetMinSize(100,100);
+		SteamClient.Window.ResizeTo(500,1080,1);
+		//Remove the majority of window pane
+			var style = document.createElement('style'),
+	script = document.getElementsByTagName('script')[0],
+		styles = '[class*="desktopbrowser_URLBar_"]{margin-top: -34px;border:2px solid #141920} .title-area-children{	display:none!important}.title-area {height:0}[class*="steamdesktopoverlay_OverlayPopup"][class*="steamdesktopoverlay_Resizable"]{height:100% !important}';
+	script.parentNode.insertBefore(style, script);
+	try{style.innerHTML = styles;}
+	//IE fix
+	catch(error){style.styleSheet.cssText = styles;}
+	}
+}
+Musicsupersize();
